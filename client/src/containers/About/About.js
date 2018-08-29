bc7a4da35956477d5db5410a9e11a118fa873919
Becca K <rkopstein@gmail.com>
2018-08-28 22:55:12 -0400

    edited about page typo

--- client/src/containers/About/About.js
+++ client/src/containers/About/About.js
@@ -9,13 +9,12 @@ const About = () => (
         alt="owner"
         className="owner"
         src={require("../../images/CCireneBW.jpeg")}
-        title="Irene, the owner, and barista Garrett" 
+        title="Irene, the owner, and barista Garrett"
       />
       <p>
-
-          What started out as a dream hatched over a cup of coffee and a homemade
-        cake, and turned into a reality when Catawba Coffee came to be in 2014. When
-        Irene, the owner, came to Mount Holly it was originally for her to
+        What started out as a dream hatched over a cup of coffee and a homemade
+        cake, and turned into a reality when Catawba Coffee came to be in 2014.
+        When Irene, the owner, came to Mount Holly it was originally for her to
         manage another independent shop. As time went by she met some of the
         most wonderful people! Many who knew each other and some who just
         happened by (mostly by accident) no matter how you got here or where you
@@ -26,7 +25,7 @@ const About = () => (
       <br />
       <p>
         When the opportunity came to Irene and her husband Joe to buy the shop
-        it was an almost immediate a yes. The fear and doubt were quickly pushed
+        it was nearly an immediate yes. The fear and doubt were quickly pushed
         aside when the thought of having to leave Mount Holly and the family
         that they'd became a part of was no longer imminent. Often times they're
         asked how they came up with the name of their shop and the answer is and
@@ -34,10 +33,8 @@ const About = () => (
         being business owners was accepted and they have never looked back.
       </p>
       <br />
-      <p id= "last">
-
-          So, If you're in the area be sure to stop by for a cup and a smile. We'd
-
+      <p id="last">
+        So, If you're in the area be sure to stop by for a cup and a smile. We'd
         love to meet you!
       </p>
     </div>
